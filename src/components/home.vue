11ad50916e2d307bee3e4f349bba88b5c7b2249c
ruby <549426210@qq.com>
2019-10-15 17:50:14 +0800

    second commit

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h3>home</h3>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    h3 {
+        color: #2b81af;
+    }
+</style>
---
+++ src/components/member.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>member</h3>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    h3 {
+        color: #2b81af;
+    }
+</style>
