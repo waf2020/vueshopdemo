21f399d2d69936e11fe804b89f53e9a6f65393d0
一见你就笑 <2693588140@qq.com>
2019-10-17 22:08:02 +0800

    '2019-10-17

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,41 @@
 <template>
 <div>
-    <h3>home</h3>
+
     <mt-swipe :auto="4000">
-        <mt-swipe-item>1</mt-swipe-item>
-        <mt-swipe-item>2</mt-swipe-item>
-        <mt-swipe-item>3</mt-swipe-item>
+        <mt-swipe-item><img src="../images/noe.jpg" width=""></mt-swipe-item>
+        <mt-swipe-item><img src="../images/two.jpg"></mt-swipe-item>
+        <mt-swipe-item><img src="../images/three.jpg"></mt-swipe-item>
     </mt-swipe>
+    <!--icon区域-->
+    <div class="icon-container">
+        <div class="fa-items">
+            <img src="../images/icon-sale.png">
+            <span>拍卖</span>
+        </div>
+        <div class="fa-items">
+            <img src="../images/icon-classfity.png">
+            <span>分类</span>
+        </div>
+        <div class="fa-items">
+            <img src="../images/icon-food.png">
+            <span>小车车</span>
+        </div>
+        <div class="fa-items">
+            <img src="../images/icon-money.png">
+            <span>聚划算</span>
+        </div><div class="fa-items">
+        <img src="../images/entrace.png">
+        <span>进口</span>
+    </div>
+        <div class="fa-items">
+            <img src="../images/incon-topup.png">
+            <span>充值</span>
+        </div>
+
+
+
+
+    </div>
 </div>
 </template>
 
@@ -19,15 +49,40 @@
         },
         methods:{
             getlist(){
-               this.$http.get('http://vue.studyit.io//api/getnewslist').then(response=>{
+               /*this.$http.get('http://vue.studyit.io//api/getnewslist').then(response=>{
                    console.log("response="+response);
-               })
+               })*/
             }
         }
     }
 </script>
 
 <style scoped>
+    .icon-container {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .icon-container >.fa-items{
+        width: 33%;
+    }
+    .icon-container >.fa-items >span {
+        font-family: '微软雅黑';
+        padding-top: 6px;
+        display: block;
+        text-align: center;
+    color: rgb(102, 102, 102);
+
+    }
+    .icon-container >.fa-items >img {
+        height: 75%;
+        width: 80%;
+        display: block;
+        margin: 0 auto;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
     .mint-swipe{
         height: 200px;
     }
